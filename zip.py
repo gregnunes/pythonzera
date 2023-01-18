ace96c66c335e63665c123ea9a3382dde8a48889
Gregory Nunes <gregnunes94@gmail.com>
2023-01-18 17:20:48 -0300

    Breve entendimento sobre Zip

---
+++ zip.py
@@ -0,0 +1,9 @@
+# Zip
+# Zip() ->Cria imiterável (Zip object) que agrega elemento de cada um dos iteraveis com entradas em pares.
+# Exemplos 
+
+#lista1 =[1 , 2 , 3]
+#lista2 = [4, 5 , 6]
+
+#zipl = zip(lista1, lista2)
+
